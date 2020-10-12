bb7fa530542c8fff437526cae6a5487aefcdb87b
Leon <leon.woeste@tu-dortmund.de>
2020-10-12 22:53:05 +0200

    Rechenzentrum-Klasse hinzugefügt

--- src/rechenzentrum/Rechenzentrum.java
+++ src/rechenzentrum/Rechenzentrum.java
@@ -2,4 +2,24 @@ package rechenzentrum;
 
 public class Rechenzentrum {
 
+	private String name;
+	
+	public Rechenzentrum(String name)
+	{
+		this.name = name;
+
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static void main(String[] args)
+	{
+		new Rechenzentrum("OH12");
+	}
 }
